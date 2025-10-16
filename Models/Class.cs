d66acfd1e1458f03b1f80e1e249ecd6adb6ca64e
Phan Vũ Hoàng Hải <phanhaiph999@gmail.com>
2025-10-16 19:26:28 +0700

    Quan_Ly_Sinh_Vien

--- Models/Class.cs
+++ Models/Class.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
-namespace StudentClassApi.Models
+namespace Quan_Ly_Sinh_Vien.Models
 {
     public class Class
     {
