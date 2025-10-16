4e7bfdc5e2454c443942a1d46d14b6677db43997
Nguyễn Nhật Long <nhatlong12112005@gmail.com>
2025-10-16 21:57:56 +0700

    sua loi mapping

--- Profiles/MappingProfiles.cs
+++ Profiles/MappingProfiles.cs
@@ -2,20 +2,22 @@ using AutoMapper;
 using Quan_Ly_Sinh_Vien.Models;
 using Quan_Ly_Sinh_Vien.DTOs;
 
-
 namespace Quan_Ly_Sinh_Vien.Profiles
 {
     public class MappingProfile : Profile
     {
         public MappingProfile()
         {
+            // --------- Class Mapping ----------
             CreateMap<Class, ClassDto>();
             CreateMap<CreateClassDto, Class>();
+
+            // --------- Student Mapping ----------
+            CreateMap<CreateStudentDto, Student>();
+            CreateMap<UpdateStudentDto, Student>();
+
             CreateMap<Student, StudentDto>()
                 .ForMember(dest => dest.ClassName, opt => opt.MapFrom(src => src.Class.Name));
-            CreateMap<CreateClassDto, Student>();
-            CreateMap<UpdateStudentDto, Student>()
-                .ForMember(dest => dest.ClassId, opt => opt.Ignore());
         }
     }
 }
