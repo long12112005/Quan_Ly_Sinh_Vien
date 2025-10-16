1ff4bfc01054e19be841f95b31609dace113fa46
Nguyễn Nhật Long <nhatlong12112005@gmail.com>
2025-10-16 21:24:24 +0700

    .

--- Profiles/MappingProfiles.cs
+++ Profiles/MappingProfiles.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using Quan_Ly_Sinh_Vien.Models;
 using Quan_Ly_Sinh_Vien.DTOs;
+using StudentClassApi.Dtos;
 
 namespace Quan_Ly_Sinh_Vien.Profiles
 {
@@ -12,7 +13,7 @@ namespace Quan_Ly_Sinh_Vien.Profiles
             CreateMap<CreateClassDto, Class>();
             CreateMap<Student, StudentDto>()
                 .ForMember(dest => dest.ClassName, opt => opt.MapFrom(src => src.Class.Name));
-            CreateMap<CreateStudentDto, Student>();
+            CreateMap<CreateClassDto, Student>();
             CreateMap<UpdateStudentDto, Student>()
                 .ForMember(dest => dest.ClassId, opt => opt.Ignore());
         }
